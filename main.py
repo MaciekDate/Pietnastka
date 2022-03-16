b166ffa98cc200113e6c7aa92b25c65c0c6dceb5
MaciekDate <maciekdatepl@gmail.com>
2022-03-16 18:45:48 +0100

    Added reading from file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("Wiadomosc testowa 24")
+import numpy as np
+
+print("Wiadomosc testowa 24")
+
+final_board = np.array([['1', '2', '3', '4'], ['5', '6', '7', '8'], ['9', '10', '11', '12'], ['13', '14', '15', '0']])
+depth = 20
+
+print("Ukonczona ukladanka:")
+print(final_board)
+
+problem_board = np.loadtxt("puzzle.txt", skiprows=1, dtype=int)
+print("Wczytana ukladanka:")
+print(problem_board)
