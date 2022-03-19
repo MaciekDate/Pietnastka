d3fbdc950b5da10b4412930f08e74b454dc946cb
PGralczyk <229885@edu.p.lodz.pl>
2022-03-19 19:06:45 +0100

    commit maciek + ja

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import numpy as np
 
-print("Wiadomosc testowa 24")
-
+# Boards
 final_board = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]])
-depth = 20
+problem_board = np.loadtxt("puzzle.txt", skiprows=1, dtype=int)
+
+# Maximum depth of recursion
+depth = 10
+
+# Blank space coords
 currentX = 3
 currentY = 2
 
 varX = 0
 varY = 0
 
-problem_board = np.loadtxt("puzzle.txt", skiprows=1, dtype=int)
-
-
-
-path = ""
-
+# Function to swap blank space with another in table
 def swapper(x1, y1, x2, y2): #Y IS FOR HORIZONTAL, X IS FOR DIAGONAL
     temporary = problem_board[x1, y1]
     problem_board[x1, y1] = problem_board[x2, y2]
@@ -25,6 +24,7 @@ def swapper(x1, y1, x2, y2): #Y IS FOR HORIZONTAL, X IS FOR DIAGONAL
     currentX = x2
     currentY = y2
 
+# Deep first search
 def dfs(array, origin, brake):
     global currentX
     global currentY
@@ -33,36 +33,44 @@ def dfs(array, origin, brake):
     if np.array_equal(array, final_board):
         print("is equal")
         print(array)
-        pass
+        return
     else:
         print("is not equal")
         print(array)
         print()
-    if brake > 5:
-        print("Branch exhausted")
-        print()
-    if  brake <= 5 and origin != 'L' and currentY + 1 <= 3:#ADD SPECIFIC VALUE READ FROM FILE!!!
-        swapper(currentX, currentY, currentX, currentY + 1)
-        dfs(array, 'R', brake + 1)
-        swapper(currentX, currentY, currentX, currentY - 1)
-    if brake <= 5 and origin != 'R' and currentY - 1 >= 0 :
-        swapper(currentX, currentY, currentX, currentY - 1)
-        dfs(array, 'L', brake + 1)
-        swapper(currentX, currentY, currentX, currentY + 1)
-    if brake <= 5 and origin != 'U' and currentX + 1 <= 3:  # ADD SPECIFIC VALUE READ FROM FILE!!!
-        swapper(currentX, currentY, currentX + 1, currentY)
-        dfs(array, 'D', brake + 1)
-        swapper(currentX, currentY, currentX - 1, currentY)
-    if brake <= 5 and origin != 'D' and currentX - 1 >= 0:
-        swapper(currentX, currentY, currentX - 1, currentY)
-        dfs(array, 'U', brake + 1)
-        swapper(currentX, currentY, currentX + 1, currentY)
+        if brake > depth:
+            print("Branch exhausted")
+            print()
+            return
+
+        if  origin != 'L' and currentY + 1 <= 3:#ADD SPECIFIC VALUE READ FROM FILE!!!
+            swapper(currentX, currentY, currentX, currentY + 1)
+            dfs(array, 'R', brake + 1)
+            swapper(currentX, currentY, currentX, currentY - 1)
+
+        if origin != 'R' and currentY - 1 >= 0 :
+            swapper(currentX, currentY, currentX, currentY - 1)
+            dfs(array, 'L', brake + 1)
+            swapper(currentX, currentY, currentX, currentY + 1)
+
+        if origin != 'U' and currentX + 1 <= 3:  # ADD SPECIFIC VALUE READ FROM FILE!!!
+            swapper(currentX, currentY, currentX + 1, currentY)
+            dfs(array, 'D', brake + 1)
+            swapper(currentX, currentY, currentX - 1, currentY)
+
+        if origin != 'D' and currentX - 1 >= 0:
+            swapper(currentX, currentY, currentX - 1, currentY)
+            dfs(array, 'U', brake + 1)
+            swapper(currentX, currentY, currentX + 1, currentY)
 
 
-print()
-print(problem_board)
-print("!!!!START!!!!")
-print()
+# main function
+if __name__ == '__main__':
+    print("Final board:")
+    print(final_board)
 
-dfs(problem_board, 'N', 0)
-print(problem_board)
+    print("\nCurrent board:")
+    print(problem_board)
+
+    print("\nAlgorythm start:")
+    dfs(problem_board, 'N', 0)
